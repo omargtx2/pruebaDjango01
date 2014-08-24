b2cc388dde756ca7b8c6d40a8fcd36a8dd373cc9
omargtx2 <omargtx2@yahoo.es>
2014-08-24 13:52:36 -0500

    seteand el template de boostrap prueba 2

--- app1/views.py
+++ app1/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render_to_response, get_object_or_404
 # Create your views here.
 def inicio(request):
    return render_to_response('index.html')
-   #return render_to_response('base.html')
+   ##return render_to_response('base.html')
